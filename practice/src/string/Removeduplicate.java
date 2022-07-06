28d18f39a868e66c351024a0d8bf90227a546997
gavha <gavha@Dell>
2022-07-06 16:51:01 +0530

    update

---
+++ practice/src/string/Removeduplicate.java
@@ -0,0 +1,35 @@
+package string;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Removeduplicate
+{
+	public static void main(String[] args) 
+    {
+       String regex = "\\b(\\w+)(?:\\W+\\1\\b)+";
+        Pattern p = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
+
+        Scanner sc = new Scanner(System.in);
+        int num = Integer.parseInt(sc.nextLine());
+        System.out.println(num);
+        while (num > 0) 
+        {
+            String input = sc.nextLine();
+            
+            Matcher m = p.matcher(input);
+            
+
+            while (m.find()) 
+            {
+                input = input.replaceAll(m.group(), m.group(1));
+            }
+            
+            System.out.println(input);
+            num--;
+        }
+        
+    }
+
+}
